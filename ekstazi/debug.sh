44b99a574c7a12cedbf5cfde3cfcb885d549b177
Peter Vilim <peterlvilim@gmail.com>
2014-10-20 00:55:49 -0500

    debug
    Former-commit-id: a0b6d2a75260fe1cbd92bb84eb3bb37083ccde12

--- ekstazi/debug.sh
+++ ekstazi/debug.sh
@@ -1,2 +1,2 @@
-export MAVEN_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8001,suspend=n"
+export MAVEN_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n"
 mvn hpi:run
