7d281faf2ce9d7f3839b819de67106f9029b8847
Peter Vilim <peterlvilim@users.noreply.github.com>
2014-11-25 04:11:39 -0600

    cleanup

--- ekstazi/src/test/java/com/pluralsight/ekstazi/BuildTests.java
+++ ekstazi/src/test/java/com/pluralsight/ekstazi/BuildTests.java
@@ -1,20 +1,12 @@
 package com.pluralsight.ekstazi;
 
 import org.jvnet.hudson.test.JenkinsRule;
-import org.apache.commons.io.FileUtils;
-import hudson.matrix.MatrixProject;
 import hudson.maven.MavenModuleSet;
-import hudson.model.FreeStyleProject;
-import hudson.model.*;
-import hudson.maven.*;
 import hudson.matrix.*;
 import hudson.scm.*;
-import hudson.tasks.Shell;
 
 import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.jvnet.hudson.test.HudsonTestCase;
 
 public class BuildTests {
   @Rule public JenkinsRule j = new JenkinsRule();
@@ -23,15 +15,5 @@ public class BuildTests {
    j.configureMaven3();
    project.setScm(new SubversionSCM("http://svn.apache.org/repos/asf/commons/proper/lang/trunk"));
    j.buildAndAssertSuccess(project);
-   // project.getBuildClass().buildAndAssertSuccess();
-    // FreeStyleProject project = j.createFreeStyleProject();
-    // j.
-    // project.getBuildersList().add(new Shell("echo hello"));
-    // MavenBuild build = project.scheduleBuild2(0).get();
-
-    // System.out.println(build.getDisplayName() + " completed");
-    // String s = FileUtils.readFileToString(build.getLogFile());
-    // System.out.println("asdf");
-    // assertFalse(s.contains("+ echo hello"));
   }
 }
