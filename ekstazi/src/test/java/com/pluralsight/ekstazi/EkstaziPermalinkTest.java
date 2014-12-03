4309a50e459eff7c741b9045ed472ca7d55565da
aditya <aditya.ahuja123@gmail.com>
2014-12-03 03:11:25 -0600

    updated: super-minor refactoring

--- ekstazi/src/test/java/com/pluralsight/ekstazi/EkstaziPermalinkTest.java
+++ ekstazi/src/test/java/com/pluralsight/ekstazi/EkstaziPermalinkTest.java
@@ -6,9 +6,10 @@ import org.junit.Test;
 public class EkstaziPermalinkTest {
 
     public static final String DISPLAY_NAME = "Last Successful Ekstazi Build";
+    public static final int    BUILD_NUMER  = 3;
 
     @Test(expected = IllegalArgumentException.class)
     public void disallowNullName() {
-        new EkstaziPermalink(3, null, DISPLAY_NAME);
+        new EkstaziPermalink(BUILD_NUMER, null, DISPLAY_NAME);
     }
 }
